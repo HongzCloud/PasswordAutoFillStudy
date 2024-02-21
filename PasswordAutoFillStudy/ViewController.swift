d65a5d698c1d8f487d0cd3e9d4645258197c7594
HongzCloud <jihong21511@gmail.com>
2024-02-21 19:12:01 +0900

    :lipstick: 아이디 및 패스워드 텍스트 필드 UI 추가

--- PasswordAutoFillStudy/ViewController.swift
+++ PasswordAutoFillStudy/ViewController.swift
@@ -8,12 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setUpEmailTextField()
     }
 
-
+    func setUpEmailTextField() {
+        emailTextField.textContentType = .emailAddress
+        emailTextField.keyboardType = .emailAddress
+        passwordTextField.textContentType = .password
+        passwordTextField.isSecureTextEntry = true
+    }
 }
 
